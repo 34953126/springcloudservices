47b4429b8c759fcf4f71eb72d11766a09de30012
admin <34953126@qq.com>
2019-11-01 11:39:31 +0800

    添加8761

---
+++ eureka-server-8761/src/main/java/com/ybb/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.ybb.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by ybb on 2019/11/1.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
